50a762797c3bf4ebaf50b9ac0b4ad4ae7cfcf48e
Sridhar Ratnakumar <srid@srid.ca>
2019-06-17 18:20:49 -0400

    Initial commit

---
+++ backend/src/Backend.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+module Backend where
+
+import Common.Route
+import Obelisk.Backend
+
+backend :: Backend BackendRoute FrontendRoute
+backend = Backend
+  { _backend_run = \serve -> serve $ const $ return ()
+  , _backend_routeEncoder = backendRouteEncoder
+  }
---
+++ common/src/Common/Api.hs
@@ -0,0 +1,4 @@
+module Common.Api where
+
+commonStuff :: String
+commonStuff = "Here is a string defined in code common to the frontend and backend."
