0015248f9822eec767bf276fda28ff0a0e9ed1c9
Sridhar Ratnakumar <srid@srid.ca>
2019-06-28 09:38:45 -0400

    Simplify nix and cabal files

--- reflex-platform.nix
+++
@@ -1,6 +0,0 @@
-import ((import <nixpkgs> {}).fetchFromGitHub {
-  owner = "reflex-frp";
-  repo = "reflex-platform";
-  rev = "716879f16d53c93766e7ed9af17416fccb2edfe1";
-  sha256 = "1mngxa24cfpvxxq4hgh77nw36vny4abl5wi2xmlwpkk25wzm0h0x";
-})
