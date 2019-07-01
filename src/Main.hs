814bbdb26f5f5d6aaebed66d81e8ab82a9e3e299
Sridhar Ratnakumar <srid@srid.ca>
2019-07-01 09:15:38 -0400

    Add Rib.App, simplifying Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -4,39 +4,13 @@
 
 module Main where
 
-import Control.Concurrent (threadDelay)
-import Control.Concurrent.Async (concurrently_)
-import Control.Monad (forever, void, when)
 import Data.Default (def)
 
-import System.Console.CmdArgs (Data, Typeable, auto, cmdArgs, help, modes, (&=))
-import System.FSNotify (watchTree, withManager)
-
-import qualified Rib.Server as Server
 import qualified Rib.Settings as S
-import qualified Rib.Shake as Shake
+import qualified Rib.App as App
 
 import qualified HTML
 
-data App
-  = Watch
-  | Serve { port :: Int, watch :: Bool }
-  | Generate { force :: Bool }
-  deriving (Data,Typeable,Show,Eq)
-
-cli :: App
-cli = modes
-  [ Watch
-      &= help "Watch for changes and generate"
-  , Serve
-      { port = 8080 &= help "Port to bind to"
-      , watch = False &= help "Watch in addition to serving generated files"
-      } &= help "Serve the generated site"
-  , Generate
-      { force = False &= help "Force generation of all files"
-      } &= help "Generate the site"
-        &= auto  -- | Generate is the default command.
-  ]
 
 -- | Configure this site here.
 --
@@ -44,29 +18,14 @@ cli = modes
 siteSettings :: S.Settings
 siteSettings = def
   { S.pageHTML = HTML.pageHTML
-  -- ^ Use Reflex to render our pages.
+  -- ^ How to render a page type
   }
 
 main :: IO ()
-main = runApp siteSettings =<< cmdArgs cli
-
-runApp :: S.Settings -> App -> IO ()
-runApp cfg = \case
-  Watch -> withManager $ \mgr -> do
-    -- Begin with a *full* generation as the HTML layout may have been changed.
-    runApp cfg $ Generate True
-    -- And then every time a file changes under the content directory.
-    void $ watchTree mgr (S.contentDir cfg) (const True) $ const $ runApp cfg $ Generate False
-    -- Wait forever, effectively.
-    forever $ threadDelay maxBound
-
-  Serve p w -> concurrently_
-    (when w $ runApp cfg Watch)
-    $ Server.serve p $ S.destDir cfg
-
-  Generate forceGen ->
-    Shake.ribShake forceGen cfg
+main = App.run siteSettings
 
 -- | Entrypoint suited for ghcid
+--
+-- See README.md for usage
 dev :: IO ()
-dev = runApp siteSettings $ Serve 8080 True
+dev = App.runWith siteSettings $ App.Serve 8080 True
---
+++ src/Rib/App.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Rib.App
+  ( App(..)
+  , run
+  , runWith
+  ) where
+
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (concurrently_)
+import Control.Monad (forever, void, when)
+
+import System.Console.CmdArgs (Data, Typeable, auto, cmdArgs, help, modes, (&=))
+import System.FSNotify (watchTree, withManager)
+
+import qualified Rib.Server as Server
+import qualified Rib.Settings as S
+import qualified Rib.Shake as Shake
+
+data App
+  = Watch
+  | Serve { port :: Int, watch :: Bool }
+  | Generate { force :: Bool }
+  deriving (Data,Typeable,Show,Eq)
+
+cli :: App
+cli = modes
+  [ Watch
+      &= help "Watch for changes and generate"
+  , Serve
+      { port = 8080 &= help "Port to bind to"
+      , watch = False &= help "Watch in addition to serving generated files"
+      } &= help "Serve the generated site"
+  , Generate
+      { force = False &= help "Force generation of all files"
+      } &= help "Generate the site"
+        &= auto  -- | Generate is the default command.
+  ]
+
+-- | CLI entry point for running the Rib app
+run :: S.Settings -> IO ()
+run cfg = runWith cfg =<< cmdArgs cli
+
+-- | Like `run` but uses the given `App` mode instead of reading it from CLI
+-- arguments.
+runWith :: S.Settings -> App -> IO ()
+runWith cfg = \case
+  Watch -> withManager $ \mgr -> do
+    -- Begin with a *full* generation as the HTML layout may have been changed.
+    runWith cfg $ Generate True
+    -- And then every time a file changes under the content directory.
+    void $ watchTree mgr (S.contentDir cfg) (const True) $ const $ runWith cfg $ Generate False
+    -- Wait forever, effectively.
+    forever $ threadDelay maxBound
+
+  Serve p w -> concurrently_
+    (when w $ runWith cfg Watch)
+    $ Server.serve p $ S.destDir cfg
+
+  Generate forceGen ->
+    Shake.ribShake forceGen cfg
