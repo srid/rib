ff9cc6b75fbb1f044046729c45c6ab34afcf2a84
Sridhar Ratnakumar <srid@srid.ca>
2019-07-07 09:53:14 -0400

    Nixify the new Pandoc, WIP
    Breaking at skylighting compile

--- default.nix
+++ default.nix
@@ -6,9 +6,24 @@
   }) {}
 }:
 
-reflex-platform.project ({ pkgs, ... }: {
+reflex-platform.project ({ pkgs, hackGet, ... }: {
   packages = {
     rib = ./.;
+    pandoc = hackGet ./dep/pandoc;
+    hslua-module-system = hackGet ./dep/hslua-module-system;
+    hslua-module-text = hackGet ./dep/hslua-module-text;
+    ipynb = hackGet ./dep/ipynb;
+    tasty-lua = hackGet ./dep/tasty-lua;
+    hslua = hackGet ./dep/hslua;
+    skylighting = (hackGet ./dep/skylighting) + /skylighting;
+    skylighting-core = (hackGet ./dep/skylighting) + /skylighting-core;
+    cmark-gfm = hackGet ./dep/cmark-gfm-hs;
+    texmath = hackGet ./dep/texmath;
+  };
+
+  overrides = self: super: with pkgs.haskell.lib; {
+    hslua-module-system = dontCheck super.hslua-module-system;
+    hslua-module-text = dontCheck super.hslua-module-text;
   };
 
   shells = {
