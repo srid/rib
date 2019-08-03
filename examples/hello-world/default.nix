a9a1043d42b73b983a98fac153b8f34f5e0ea7bb
Sridhar Ratnakumar <srid@srid.ca>
2019-08-03 12:40:45 -0400

    Simplify examples default.nix
    By reusing rib's default.nix

--- examples/hello-world/default.nix
+++ examples/hello-world/default.nix
@@ -1,34 +1,3 @@
-let
-  pkgs = import <nixpkgs> { };
-  compiler = "default";
-  haskellPackages =
-    if compiler == "default"
-      then pkgs.haskellPackages
-      else pkgs.haskell.packages.${compiler};
-in
-haskellPackages.developPackage {
+import ./../../default.nix {
   root = ./.;
-  source-overrides = {
-    clay = pkgs.fetchFromGitHub {
-      owner = "sebastiaanvisser";
-      repo = "clay";
-      rev = "54dc9eaf0abd180ef9e35d97313062d99a02ee75";
-      sha256 = "0y38hyd2gvr7lrbxkrjwg4h0077a54m7gxlvm9s4kk0995z1ncax";
-    };
-    pandoc-include-code = pkgs.fetchFromGitHub {
-      owner = "owickstrom";
-      repo = "pandoc-include-code";
-      rev = "7e4d9d967ff3e3855a7eae48408c43b3400ae6f4";
-      sha256 = "0wvml63hkhgmmkdd2ml5a3g7cb69hxwdsjmdhdzjbqbrwkmc20rd";
-    };
-    rib = ./../..;
-  };
-
-  overrides = self: super: with pkgs.haskell.lib; {
-    clay = dontCheck super.clay;
-  };
-
-  modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
-    buildTools = (attrs.buildTools or []) ++ [haskellPackages.ghcid] ;
-  });
 }
