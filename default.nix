ca3e9fbd1befbca868d3254559805eacba78e7eb
Sridhar Ratnakumar <srid@srid.ca>
2020-01-19 17:38:59 -0500

    Cleanup and update default.nix (#91)
    * Advance nixpkgs
    
    * Remove dhall, tomland and dependent-sum from default.nix
    
    * Update path and relude
    
    * override hspec-megaparsec
    
    * Cleanup overrides
    
    * Refactor

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 let
   # Use https://howoldis.herokuapp.com/ to find the next hash to update nixpkgs to.
   # Look for the "Last updated" hash for the entry `nixpkgs-unstable`
-  nixpkgsRev = "7e8454fb856";
-in { pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz") {}
+  nixpkgsRev = "c438ce12a85";
+in { 
+  pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz") {}
 , compiler ? "default"
 , root ? ./.
 , name ? "rib"
@@ -10,34 +11,21 @@ in { pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/arch
 , ...
 }:
 let
-  haskellPackages =
-    if compiler == "default"
-      then pkgs.haskellPackages
-      else pkgs.haskell.packages.${compiler};
-  t = pkgs.lib.trivial;
-  h = pkgs.haskell.lib;
+  pipe = pkgs.lib.trivial.pipe;
+  optionals = pkgs.lib.lists.optionals;
   githubRepo = fq: rev:
     builtins.fetchTarball ("https://github.com/" + fq + "/archive/" + rev + ".tar.gz");
-
-  pp = githubRepo "quchen/prettyprinter" "v1.5.1";
-  ppUnpackSymlinks = hp: pkgs.haskell.lib.overrideCabal hp (drv: {
-    postUnpack = ''
-      cp --remove-destination ${pp}/prettyprinter/misc/version-compatibility-macros.h $sourceRoot/misc/
-      cp --remove-destination ${pp}/prettyprinter/LICENSE.md $sourceRoot/
-      cp --remove-destination ${pp}/prettyprinter/README.md $sourceRoot/
-    '';
-  });
-  justBuild = p: h.dontHaddock (h.dontCheck p);
 in
-haskellPackages.developPackage {
+pkgs.haskellPackages.developPackage {
   inherit root name;
   source-overrides = {
     rib = ./.;
-    # Override haskell packages here:
     clay =
       githubRepo "sebastiaanvisser/clay" "cc7729b";
     megaparsec =
       githubRepo "mrkkrp/megaparsec" "90b4608";
+    hspec-megaparsec =
+      githubRepo "mrkkrp/hspec-megaparsec" "365a277";
     mmark =
       githubRepo "mmark-md/mmark" "9a6a649";
     mmark-ext =
@@ -45,58 +33,25 @@ haskellPackages.developPackage {
     pandoc-include-code =
       githubRepo "owickstrom/pandoc-include-code" "7e4d9d9";
     path =
-      githubRepo "commercialhaskell/path" "1d8bdf0";
+      githubRepo "commercialhaskell/path" "4ceb12b";
     path-io =
-      githubRepo "mrkkrp/path-io" "84ce6a2";
+      githubRepo "mrkkrp/path-io" "236825b";
     relude =
-      githubRepo "kowainik/relude" "bfb5f60";
-
-    # The dependencies below are not used in rib; but useful to have for users
-    # of the library.
-    # TODO: Provide a mechanism for the user to override these.
-    dependent-sum =
-      let dsum = githubRepo "mokus0/dependent-sum" "5ab6d81"; in "${dsum}/dependent-sum";
-    some = githubRepo "phadej/some" "7e2a9ef5352097954a3a416a5ef12bc35b0d53db";  # 1.0.0.3
-
-    # Dhall, and its dependency overrides
-    dhall =
-      let dhallHaskell = githubRepo "dhall-lang/dhall-haskell" "1.28.0";
-      in "${dhallHaskell}/dhall";
-    atomic-write = githubRepo "stackbuilders/atomic-write" "v0.2.0.7";
-    generic-random = githubRepo "lysxia/generic-random" "1.3.0.0";
-    prettyprinter = "${pp}/prettyprinter";
-
-    # TOML parser
-    tomland = githubRepo "kowainik/tomland" "d9b7a1d";
+      githubRepo "kowainik/relude" "ee509c8";
   } // source-overrides;
-  overrides = self: super: {
-    clay = h.dontCheck super.clay;
-    mmark = h.dontCheck super.mmark;
-    modern-uri = h.dontCheck super.modern-uri;
-    megaparsec = h.doJailbreak (h.dontCheck super.megaparsec);  # For tomland
-    tomland = h.dontCheck super.tomland;
-    path = h.dontCheck super.path;
-    path-io = h.doJailbreak super.path-io;  # Override hardcoded dependency on path ==0.6.*
-    some = h.doJailbreak super.some;
-    relude = h.dontCheck super.relude;
-    prettyprinter = h.dontCheck (ppUnpackSymlinks super.prettyprinter);
-    dhall = h.dontCheck super.dhall;
-    rib = justBuild super.rib;
-  };
-  modifier =
+  modifier = with pkgs.haskell.lib;
     let
-      platformSpecificDeps =
-        if builtins.currentSystem == "x86_64-linux"
-        then [pkgs.fsatrace]
-        else []; # fsatrace disabled on macOS, and requires sytem configuration.
-      addExtraDeps =
-        (t.flip h.addBuildTools) (with haskellPackages;
+      addRibDeps = drv:
+        addBuildTools drv (with pkgs.haskellPackages;
           [ cabal-install
             ghcid
-          ] ++ platformSpecificDeps
+          ] 
+          # Shake recommends fsatrace, but it requires system configuration on
+          # macOS.
+          ++ optionals (builtins.currentSystem == "x86_64-linux") [pkgs.fsatrace]
         );
-    in (t.flip t.pipe) [
-      addExtraDeps
-      h.dontHaddock
+    in drv: pipe drv [
+      addRibDeps
+      dontHaddock
     ];
 }
