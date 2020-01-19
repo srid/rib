9a68bda1e844b83a0814553f4ede32179c35bfa0
Sridhar Ratnakumar <srid@srid.ca>
2020-01-19 10:43:02 -0500

    Remove tomland and dependent-sum
    Also override hspec-megaparsec

--- default.nix
+++ default.nix
@@ -19,12 +19,12 @@ pkgs.haskellPackages.developPackage {
   inherit root name;
   source-overrides = {
     rib = ./.;
-
-    # Override haskell packages here:
     clay =
       githubRepo "sebastiaanvisser/clay" "cc7729b";
     megaparsec =
       githubRepo "mrkkrp/megaparsec" "90b4608";
+    hspec-megaparsec =
+      githubRepo "mrkkrp/hspec-megaparsec" "365a277";
     mmark =
       githubRepo "mmark-md/mmark" "9a6a649";
     mmark-ext =
@@ -37,24 +37,7 @@ pkgs.haskellPackages.developPackage {
       githubRepo "mrkkrp/path-io" "236825b";
     relude =
       githubRepo "kowainik/relude" "ee509c8";
-
-    # The dependencies below are not used in rib; but useful to have for users
-    # of the library.
-    # TODO: Move these to rib-sample, as a commented out example.
-    dependent-sum =
-      let dsum = githubRepo "mokus0/dependent-sum" "5ab6d81"; 
-      in "${dsum}/dependent-sum";
-    some = githubRepo "phadej/some" "7e2a9ef5352097954a3a416a5ef12bc35b0d53db";  # 1.0.0.3
-    # TOML parser
-    tomland = githubRepo "kowainik/tomland" "d9b7a1d";
   } // source-overrides;
-  overrides = self: super: with pkgs.haskell.lib; {
-    mmark = dontCheck super.mmark;  # Test deps use wrong megaparsec version
-    modern-uri = dontCheck super.modern-uri;  # Test deps use wrong megaparsec version
-
-    some = doJailbreak super.some;
-    tomland = dontCheck super.tomland;
-  };
   modifier = with pkgs.haskell.lib;
     let
       platformSpecificDeps =
