6148df9fbdee4e55b5bf9d7ffe3916b3e3b95e96
Sridhar Ratnakumar <srid@srid.ca>
2020-03-01 22:10:21 -0500

    Add routeUrlRel

--- src/Rib/Route.hs
+++ src/Rib/Route.hs
@@ -8,6 +8,7 @@
 module Rib.Route
   ( IsRoute (..),
     routeUrl,
+    routeUrlRel,
     writeRoute,
   )
 where
@@ -27,18 +28,29 @@ import Rib.Shake (writeFileCached)
 class IsRoute (r :: Type -> Type) where
   routeFile :: MonadThrow m => r a -> m (Path Rel File)
 
--- | Get the URL to a route
+data UrlType = Relative | Absolute
+
+path2Url :: Path Rel File -> UrlType -> Text
+path2Url fp = toText . toFilePath . \case
+  Relative ->
+    fp
+  Absolute ->
+    [absdir|/|] </> fp
+
 routeUrl :: IsRoute r => r a -> Text
-routeUrl = urlForPath . either (error . toText . displayException) id . routeFile
+routeUrl = routeUrl' Absolute
+
+routeUrlRel :: IsRoute r => r a -> Text
+routeUrlRel = routeUrl' Relative
+
+-- | Get the URL to a route
+routeUrl' :: IsRoute r => UrlType -> r a -> Text
+routeUrl' urlType = stripIndexHtml . flip path2Url urlType . either (error . toText . displayException) id . routeFile
   where
-    urlForPath :: Path Rel File -> Text
-    urlForPath = stripIndexHtml . relPathToUrl
-      where
-        relPathToUrl = toText . toFilePath . ([absdir|/|] </>)
-        stripIndexHtml s =
-          if T.isSuffixOf "index.html" s
-            then T.dropEnd (T.length $ "index.html") s
-            else s
+    stripIndexHtml s =
+      if T.isSuffixOf "index.html" s
+        then T.dropEnd (T.length $ "index.html") s
+        else s
 
 writeRoute :: (IsRoute r, ToString s) => r a -> s -> Action ()
 writeRoute r content = do
