5ab4a4e07eb02746719b116e442b57e99808f7f0
Sridhar Ratnakumar <srid@srid.ca>
2020-04-09 18:27:16 -0400

    Refactor ApplicativeDo

--- src/Rib/Cli.hs
+++ src/Rib/Cli.hs
@@ -1,12 +1,9 @@
 {-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Rib.Cli
   ( CliConfig (..),
@@ -18,6 +15,7 @@ import Development.Shake (Verbosity (..))
 import Options.Applicative
 import Path
 import Relude
+import Relude.Extra.Tuple
 
 -- Rib's CLI configuration
 --
@@ -46,17 +44,57 @@ data CliConfig
 
 cliParser :: Path Rel Dir -> Path Rel Dir -> Parser CliConfig
 cliParser inputDirDefault outputDirDefault = do
-  rebuildAll <- switch (long "rebuild-all" <> help "Rebuild all sources")
-  watch <- switch (long "watch" <> short 'w' <> help "Watch for changes and regenerate")
+  rebuildAll <-
+    switch
+      ( long "rebuild-all"
+          <> help "Rebuild all sources"
+      )
+  watch <-
+    switch
+      ( long "watch"
+          <> short 'w'
+          <> help "Watch for changes and regenerate"
+      )
   serve <-
     optional
-      (option auto (long "serve" <> short 's' <> metavar "PORT" <> help "Run a HTTP server on the generated directory"))
-  verbosity <- fmap (bool Verbose Silent) (switch (long "quiet" <> help "Log nothing"))
-  inputDir <- relDirOption (long "input-dir" <> metavar "INPUTDIR" <> value (toFilePath inputDirDefault) <> help ("Directory containing the source files (" <> "default: " <> toFilePath inputDirDefault <> ")"))
-  outputDir <- relDirOption (long "output-dir" <> metavar "OUTPUTDIR" <> value (toFilePath outputDirDefault) <> help ("Directory where files will be generated (" <> "default: " <> toFilePath outputDirDefault <> ")"))
+      ( option
+          auto
+          ( long "serve"
+              <> short 's'
+              <> metavar "PORT"
+              <> help "Run a HTTP server on the generated directory"
+          )
+      )
+  verbosity <-
+    fmap
+      (bool Verbose Silent)
+      ( switch
+          ( long "quiet"
+              <> help "Log nothing"
+          )
+      )
+  ~(inputDir, shakeDbDir) <-
+    fmap (mapToSnd shakeDbDirFrom) $
+      relDirOption
+        ( long "input-dir"
+            <> metavar "INPUTDIR"
+            <> value (toFilePath inputDirDefault)
+            <> help ("Directory containing the source files (" <> "default: " <> toFilePath inputDirDefault <> ")")
+        )
+  outputDir <-
+    relDirOption
+      ( long "output-dir"
+          <> metavar "OUTPUTDIR"
+          <> value (toFilePath outputDirDefault)
+          <> help ("Directory where files will be generated (" <> "default: " <> toFilePath outputDirDefault <> ")")
+      )
+  pure CliConfig {..}
+  where
+    relDirOption = fmap (either (error . toText . displayException) id . parseRelDir) . strOption
+
+shakeDbDirFrom :: Path Rel Dir -> Path Rel Dir
+shakeDbDirFrom inputDir =
   -- Keep shake database directory under the src directory instead of the
   -- (default) current working directory, which may not always be a project
   -- root (as in the case of neuron).
-  pure $ CliConfig rebuildAll watch serve verbosity inputDir outputDir (inputDir </> [reldir|.shake|])
-  where
-    relDirOption = fmap (either (error . toText . displayException) id . parseRelDir) . strOption
+  inputDir </> [reldir|.shake|]
