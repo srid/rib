c623ff953d1d65613a1a2581c4a676b00bf94f35
Sridhar Ratnakumar <srid@srid.ca>
2020-05-11 22:27:24 -0400

    Run ormolu

--- src/Rib/Extra/OpenGraph.hs
+++ src/Rib/Extra/OpenGraph.hs
@@ -21,16 +21,15 @@ import qualified Text.URI as URI
 -- The OpenGraph metadata
 --
 -- This type can be directly rendered to HTML using `toHTML`.
-data OpenGraph
-  = OpenGraph
-      { _openGraph_title :: Text,
-        _openGraph_url :: Maybe URI.URI,
-        _openGraph_author :: Maybe Text,
-        _openGraph_description :: Maybe Text,
-        _openGraph_siteName :: Text,
-        _openGraph_type :: Maybe OGType,
-        _openGraph_image :: Maybe URI.URI
-      }
+data OpenGraph = OpenGraph
+  { _openGraph_title :: Text,
+    _openGraph_url :: Maybe URI.URI,
+    _openGraph_author :: Maybe Text,
+    _openGraph_description :: Maybe Text,
+    _openGraph_siteName :: Text,
+    _openGraph_type :: Maybe OGType,
+    _openGraph_image :: Maybe URI.URI
+  }
   deriving (Eq, Show)
 
 instance ToHtml OpenGraph where
@@ -66,14 +65,13 @@ instance ToHtml OGType where
       metaOg "type" "website"
 
 -- TODO: _article_profile :: [Profile]
-data Article
-  = Article
-      { _article_section :: Maybe Text,
-        _article_modifiedTime :: Maybe UTCTime,
-        _article_publishedTime :: Maybe UTCTime,
-        _article_expirationTime :: Maybe UTCTime,
-        _article_tag :: [Text]
-      }
+data Article = Article
+  { _article_section :: Maybe Text,
+    _article_modifiedTime :: Maybe UTCTime,
+    _article_publishedTime :: Maybe UTCTime,
+    _article_expirationTime :: Maybe UTCTime,
+    _article_tag :: [Text]
+  }
   deriving (Eq, Show)
 
 instance ToHtml Article where
