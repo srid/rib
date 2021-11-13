fc40379cd0edda603f4355bd67df415d52e402ff
Jonathan Reeve <jonathan.reeve@columbia.edu>
2021-11-12 22:14:03 -0500

    add new package to flake

--- flake.nix
+++ flake.nix
@@ -34,8 +34,31 @@
           lucid = pkgs.haskell.lib.dontCheck super.lucid;
         };
         source-overrides = {
-          rib = ./rib;
+          # rib = ./rib;
           rib-core = ./rib-core;
+        };
+      };
+    packages.x86_64-linux.rib-core =
+      with import nixpkgs { system = "x86_64-linux"; config = { allowBroken = true; doCheck = false; }; };
+      pkgs.haskellPackages.developPackage {
+        root = ./rib-core;
+        name = "rib-core";
+        modifier = drv:
+          pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+            [
+              cabal-install cabal-fmt pkgs.nixpkgs-fmt
+              ghcid ormolu haskell-language-server zlib
+            ]);
+        overrides = self: super: {
+          mmark-ext = pkgs.haskell.lib.dontCheck super.mmark-ext;
+          mmark = pkgs.haskell.lib.dontCheck super.mmark;
+          pandoc-types = pkgs.haskell.lib.dontCheck super.pandoc-types;
+          aeson = pkgs.haskell.lib.dontCheck super.aeson;
+          lucid = pkgs.haskell.lib.dontCheck super.lucid;
+        };
+        source-overrides = {
+          # rib = ./rib;
+          # rib-core = ./rib-core;
         };
       };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.rib;
