7ef1abbf4446a731e56832e5f53cc4125254b508
Jonathan Reeve <jonathan.reeve@columbia.edu>
2021-11-12 23:10:35 -0500

    trying some more flake voodoo

--- flake.nix
+++ flake.nix
@@ -2,36 +2,28 @@
   description = "Rib, a static site generator based on Shake.";
   inputs = {
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-    # rib = { url = "."; flake = false; };
-    # Pin nixpkgs
-    # nixpkgs = { url = "github:NixOS/nixpkgs/5272327b81ed355bbed5659b8d303cf2979b6953"; flake = false; }; # 20.03
-    # nixpkgs = { url = "github:NixOS/nixpkgs/cd63096d6d887d689543a0b97743d28995bc9bc3"; flake = false; }; # 20.09
-    # nixpkgs = { url = "github:NixOS/nixpkgs/7e9b0dff974c89e070da1ad85713ff3c20b0ca97"; flake = false; }; # 21.05
-    nixpkgs = { url = "github:NixOS/nixpkgs"; flake = false; }; # Master
+    # Pin nixpkgs to nixos-unstable as of 2021-11-12
+    nixpkgs = { url = "github:NixOS/nixpkgs/92204897f3d80e7034168d883741dffae99ede5c"; };
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, ... }@inputs: {
     packages.x86_64-linux.rib =
-      with import nixpkgs { system = "x86_64-linux"; config = { allowBroken = true; doCheck = false; }; };
-      pkgs.haskellPackages.developPackage {
+      let pkgs = import nixpkgs { system = "x86_64-linux"; config = { allowBroken = true; }; };
+      in pkgs.haskellPackages.developPackage {
         root = ./rib;
         name = "rib";
         modifier = drv:
           pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
             [
-              cabal-install
-              cabal-fmt
-              pkgs.nixpkgs-fmt
-              ghcid
-              ormolu
-              haskell-language-server
-              zlib
+              cabal-install cabal-fmt pkgs.nixpkgs-fmt ghcid
+              ormolu haskell-language-server zlib
             ]);
-        overrides = self: super: {
-          mmark-ext = pkgs.haskell.lib.dontCheck super.mmark-ext;
-          mmark = pkgs.haskell.lib.dontCheck super.mmark;
-          pandoc-types = pkgs.haskell.lib.dontCheck super.pandoc-types;
-          aeson = pkgs.haskell.lib.dontCheck super.aeson;
-          lucid = pkgs.haskell.lib.dontCheck super.lucid;
+        overrides = self: super: with pkgs.haskell.lib; {
+          mmark-ext = dontCheck super.mmark-ext;
+          mmark = dontCheck super.mmark;
+          pandoc-types = dontCheck super.pandoc-types;
+          aeson = dontCheck super.aeson;
+          lucid = dontCheck super.lucid;
         };
         source-overrides = {
           # rib = ./rib;
@@ -39,8 +31,8 @@
         };
       };
     packages.x86_64-linux.rib-core =
-      with import nixpkgs { system = "x86_64-linux"; config = { allowBroken = true; doCheck = false; }; };
-      pkgs.haskellPackages.developPackage {
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.haskellPackages.developPackage {
         root = ./rib-core;
         name = "rib-core";
         modifier = drv:
@@ -49,19 +41,19 @@
               cabal-install cabal-fmt pkgs.nixpkgs-fmt
               ghcid ormolu haskell-language-server zlib
             ]);
-        overrides = self: super: {
-          mmark-ext = pkgs.haskell.lib.dontCheck super.mmark-ext;
-          mmark = pkgs.haskell.lib.dontCheck super.mmark;
-          pandoc-types = pkgs.haskell.lib.dontCheck super.pandoc-types;
-          aeson = pkgs.haskell.lib.dontCheck super.aeson;
-          lucid = pkgs.haskell.lib.dontCheck super.lucid;
+        overrides = self: super: with pkgs.haskell.lib; {
+          mmark-ext = dontCheck super.mmark-ext;
+          mmark = dontCheck super.mmark;
+          pandoc-types = dontCheck super.pandoc-types;
+          aeson = dontCheck super.aeson;
+          lucid = dontCheck super.lucid;
         };
         source-overrides = {
-          # rib = ./rib;
+          rib = ./rib;
           # rib-core = ./rib-core;
         };
       };
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.rib;
-    devShell = true;
+    devShell = self.packages.x86_64-linux.rib;
   };
 }
