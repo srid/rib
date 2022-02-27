d973a70eb8c7c61fcb6e644048363a8907d20554
Jonathan Reeve <jonathan.reeve@columbia.edu>
2022-02-27 11:39:30 -0500

    upgrade some packages

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
   # Use https://status.nixos.org// to find the next hash to update nixpkgs to.
   # Look for the "Last updated" commit hash for the entry `nixpkgs-unstable`
-  nixpkgsRev = "502845c3e31e";
+  nixpkgsRev = "53c97404221a9956165bc876b5c476d97ee3d074";
   nixpkgsSrc = builtins.fetchTarball {
     url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz";
-    sha256 = "0fcqpsy6y7dgn0y0wgpa56gsg0b0p8avlpjrd79fp4mp9bl18nda";
+    sha256 = "0228c7zdg073k2dvvvbw6ysx6gnwnlz1364rif14k08k75avm49h";
   };
 
   gitignoreSrc = builtins.fetchTarball { 
@@ -26,14 +26,28 @@ in {
 }:
 let
   optionals = pkgs.lib.lists.optionals;
+  myMmark = pkgs.haskell.lib.dontCheck pkgs.haskellPackages.mmark;
 in
 compiler.developPackage {
   inherit root name;
   source-overrides = {
     rib = ribRoot + "/rib";
     rib-core = ribRoot + "/rib-core";
+    lucid = pkgs.fetchFromGitHub {
+      owner = "chrisdone";
+      repo = "lucid";
+      rev = "090fd7dbc6c10269a50cc0f37a1bb40effed730e";
+      sha256 = "EUthRB53XrqkWMJhGZpqhJOtwGKMK1UzAE6QZ2sDuI0=";
+    };
   } // source-overrides;
   overrides = self: super: with pkgs.haskell.lib; {
+    mmark = pkgs.haskell.lib.dontCheck pkgs.haskellPackages.mmark;
+    mmark-ext = pkgs.haskellPackages.callCabal2nix "mmark-ext" (pkgs.fetchFromGitHub {
+         owner = "mmark-md";
+         repo = "mmark-ext";
+         rev = "bb7f26f8c2cf98a54472372601ce5f6d5cb9df2d";
+         sha256 = "AoXLb2/nCNgJGUeLHaZA9nfIw0b249USNEwvYn0A3uE=";
+    });
   } // (overrides self super);
   modifier = with pkgs.haskell.lib;
     let
